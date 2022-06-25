5ae72a85a4f61625732f20f8b2a0c9c3f6ec2f5d
Espressomilk <Espressomilk@users.noreply.github.com>
2022-06-24 22:31:45 -0700

    Implemented level 2

--- main.py
+++ main.py
@@ -8,27 +8,25 @@ from config import configure_config
 
 class Lost_bot():
 
-    # Static propeties
-    # wincapture = WindowCapture("LOST ARK (64-bit, DX11) v.2.3.4.1")
-    # translate_pos = wincapture.get_screen_position
-    # global screenshot
-    # screenshot = wincapture.get_screenshot()
-    # window_size = (wincapture.w, wincapture.h)
-    # print(type(screenshot))
-    # print((screenshot.shape))
-
-    wincapture = WindowCapture("TEST", True)
+    global screenshot
+    try:
+        wincapture = WindowCapture("LOST ARK (64-bit, DX11) v.2.3.5.1")
+        translate_pos = wincapture.get_screen_position
+        screenshot = wincapture.get_screenshot()
+        window_size = (wincapture.w, wincapture.h)
+    except:
+        wincapture = WindowCapture("TEST", True)
+        screenshot = None
 
     IN_CITY = True
     MOVE = False
-    CAST = False
-    BOT_WORKING = False
-    SHOULD_EXIT = False
-    TIMEOUT = 600  # seconds.
+    TIMEOUT = 250  # seconds.
     available_spells = []
-    method = cv2.TM_CCORR_NORMED
 
-    resurection_path = "images\\base_res.png"
+    RUN_COUNT = 0
+    RESURRECT_COUNT = 0
+
+    resurrection_path = "images\\base_res.jpg"
     in_game_icon_path = "images\\in_game_icon.jpg"
     portal_path = "images\\portal.jpg"
     minimap_boss_path = "images\\minimap_boss.jpg"
@@ -36,8 +34,9 @@ class Lost_bot():
     minimap_portal_path = "images\\minimap_portal.jpg"
     minimap_self_east_path = "images\\minimap_self_east.jpg"
     minimap_self_north_path = "images\\minimap_self_north.jpg"
-
-    not_in_game_count = 0
+    minimap_crystal_tower_path = "images\\minimap_crystal_tower.jpg"
+    level_2_path = "images\\level_2.jpg"
+    blue_portal_path = "images\\blue_portal.jpg"
 
     text_file = open("config.txt", "r")
     lines = text_file.read().split('\n')
@@ -46,6 +45,16 @@ class Lost_bot():
             all_spells = row.replace("spells =","").replace(" ","")
         elif "spell/time" in row:
             duration = row[15:].split(" ")
+
+    def __init__(self, test_screenshot = []):
+        self.should_exit = False
+        self.target_direction = None
+        self.not_in_game_count = 0
+        self.is_level_2 = False
+        self.disable_cast = False
+        if test_screenshot != []:
+            global screenshot
+            screenshot = test_screenshot
 
     def waitRandomizedTime(self, min, max):
         time.sleep(random.uniform(min, max))
@@ -69,7 +78,7 @@ class Lost_bot():
         return [[66, 308], [1602, 1883]]
 
     def get_minimap_center(self):
-        return (1742, 162)
+        return (1743, 184)
 
     def get_minimap(self):
         offset = self.get_minimap_offset()
@@ -79,11 +88,10 @@ class Lost_bot():
         target = cv2.imread(target_path, cv2.IMREAD_UNCHANGED)
         result = cv2.matchTemplate(self.get_minimap(), target, method)
         _, maxVal, _, maxLoc = cv2.minMaxLoc(result)
-        print(maxVal, target_path, maxLoc)
-        print(type(maxLoc))
         absoluteLoc = (maxLoc[0] + self.get_minimap_offset()[1][0] + target.shape[0] // 2, 
                        maxLoc[1] + self.get_minimap_offset()[0][0] + target.shape[1] // 2)
         if maxVal > threshold:
+            print(maxVal, target_path)
             return self.wincapture.get_screen_position(absoluteLoc)
         else:
             return None
@@ -92,24 +100,82 @@ class Lost_bot():
         target = cv2.imread(target_path, cv2.IMREAD_UNCHANGED)
         result = cv2.matchTemplate(screenshot, target, method)
         _, maxVal, _, maxLoc = cv2.minMaxLoc(result)
-        print(maxVal, target_path)
         if maxVal > threshold:
             return self.wincapture.get_screen_position(maxLoc)
         else:
             return None
 
-    def get_heading_direction(self, my_loc, target_loc, multifier=2):
+    def calc_distance(self, loc1, loc2):
+        print(loc1, loc2)
+        return math.sqrt((loc1[0] - loc2[0]) ** 2 + (loc1[1] - loc2[1]) ** 2)
+
+    def get_heading_direction(self, my_loc, target_loc, multifier=3):
         my_real_loc = self.get_center_loc()
+        print(target_loc[0] - my_loc[0], target_loc[1] - my_loc[1])
         return (my_real_loc[0] + (target_loc[0] - my_loc[0]) * multifier,
                 my_real_loc[1] + (target_loc[1] - my_loc[1]) * multifier)
 
     def scan_for_portal(self):
+        print("Portal scanning registered...")
         while not self.IN_CITY:
-            pos = self.find_pos(self.portal_path, 0.90, cv2.TM_CCORR_NORMED)
-            if pos:
+            portal_pos = self.find_pos(self.portal_path, 0.90, cv2.TM_CCORR_NORMED)
+            if portal_pos:
                 print("Portal appeared!")
-                self.SHOULD_EXIT = True
-                break
+                self.should_exit = True
+
+            # blue_portal_pos = self.find_pos(self.blue_portal_path, 0.90, cv2.TM_CCORR_NORMED)
+            # if blue_portal_pos:
+            #     print("Portal in sight!")
+            #     pyautogui.moveTo(blue_portal_pos[0], blue_portal_pos[1], random.uniform(0.1, 0.3))
+            #     pyautogui.click(button='right')
+            #     self.waitRandomizedTime(1, 2)
+            #     self.should_exit = True
+        print("Portal scanning ends...")
+    
+    def check_disable_cast(self, self_minimap_pos, target_minimap_pos):
+        if self.calc_distance(self_minimap_pos, target_minimap_pos) > 50:
+            if not self.disable_cast:
+                print("Target too far. Disable casting...")
+            self.disable_cast = True
+        else:
+            if self.disable_cast:
+                print("Target nearby. Enable casting...")
+            self.disable_cast = False
+
+
+    def scan_minimap(self):
+        print("Minimap scanning registered...")
+        while not self.IN_CITY:
+            boss_minimap_pos = self.find_on_minimap(self.minimap_boss_path, 0.68, cv2.TM_CCOEFF_NORMED)
+            elite_minimap_pos = self.find_on_minimap(self.minimap_elite_path, 0.68, cv2.TM_CCOEFF_NORMED)
+            portal_minimap_pos = self.find_on_minimap(self.minimap_portal_path, 0.68, cv2.TM_CCOEFF_NORMED)
+            self_minimap_pos = self.get_minimap_center()
+            crystal_tower_minimap_pos = self.find_on_minimap(self.minimap_crystal_tower_path, 0.68, cv2.TM_CCOEFF_NORMED)
+            if crystal_tower_minimap_pos:
+                print("Accidentally entered third floor. Exiting now...")
+                self.should_exit = True
+                self.is_level_2 = True
+            elif elite_minimap_pos:
+                print("Elite found on minimap at", elite_minimap_pos)
+                self.target_direction = self.get_heading_direction(self_minimap_pos, elite_minimap_pos)
+                self.is_level_2 = True
+                self.check_disable_cast(self_minimap_pos, elite_minimap_pos)
+            elif boss_minimap_pos:
+                print("Boss found on minimap at", boss_minimap_pos)
+                self.target_direction = self.get_heading_direction(self_minimap_pos, boss_minimap_pos)
+                self.is_level_2 = True
+                self.check_disable_cast(self_minimap_pos, boss_minimap_pos)
+            elif portal_minimap_pos:
+                print("Portal found on minimap at", portal_minimap_pos)
+                self.target_direction = self.get_heading_direction(self_minimap_pos, portal_minimap_pos)
+                self.check_disable_cast(self_minimap_pos, portal_minimap_pos)
+                if self.is_level_2:
+                    self.should_exit = True
+            else:
+                self.target_direction = None
+                self.disable_cast = False
+            time.sleep(0.5)
+        print("Minimap scanning ends...")
 
     def pos_dirs(self, index):
         x = [959, 960, 1034, 1049, 1024, 953, 886, 862, 884]
@@ -130,27 +196,39 @@ class Lost_bot():
         else: 
             cast = self.available_spells[randint(0,len(self.available_spells)-1)]
         
-            random_pos = self.pos_dirs(randint(0, 8))
-            pyautogui.moveTo(random_pos[0] + randint(-5, 5), random_pos[1] + randint(-5, 5))
+            if not self.target_direction:
+                random_pos = self.pos_dirs(randint(0, 8))
+                pyautogui.moveTo(random_pos[0] + randint(-5, 5), random_pos[1] + randint(-5, 5), random.uniform(0.1, 0.3))
+                if self.is_level_2 and randint(0, 1):
+                    # Wondering more in second floor to avoid stuck.
+                    pyautogui.mouseDown(button='right')
+                    self.waitRandomizedTime(0.5, 1)
+                    pyautogui.mouseUp(button='right')
 
             pyautogui.keyDown(cast)
-            self.waitRandomizedTime(hold_time*0.8,hold_time)
+            self.waitRandomizedTime(0.07, 0.15)
             pyautogui.keyUp(cast)
+            self.waitRandomizedTime(0.07, 0.15)
             # Double click.
             pyautogui.keyDown(cast)
-            self.waitRandomizedTime(hold_time*0.8,hold_time)
+            self.waitRandomizedTime(0.07, 0.15)
             pyautogui.keyUp(cast)
             self.available_spells.remove(cast)
             if cast == 'r' or cast == 'f':
                 self.waitRandomizedTime(1.3, 1.5)
             else:
-                self.waitRandomizedTime(0.2,1)
+                self.waitRandomizedTime(0.5, 1)
 
-    def check_resurection(self):
-        x = self.find_pos(self.resurection_path, 0.98, cv2.TM_CCORR_NORMED)
+    def ressurrection_range(self):
+        loc = [randint(1266, 1467), randint(428, 466)]
+        return self.wincapture.get_screen_position(loc)
+
+    def check_resurrection(self):
+        x = self.find_pos(self.resurrection_path, 0.95, cv2.TM_CCORR_NORMED)
         if x:
-            pyautogui.moveTo(x[0] + randint(5,25), x[1 + randint(5, 10)], random.uniform(0.2, 0.8))
-            pyautogui.click()
+            print("Resurrecting!")
+            self.moveToClick(self.ressurrection_range())
+            self.RESURRECT_COUNT += 1
 
     def is_in_game(self):
         icon = self.find_pos(self.in_game_icon_path, 0.99, cv2.TM_CCORR_NORMED)
@@ -220,31 +298,74 @@ class Lost_bot():
 
         self.check_accept()
         self.IN_CITY = False
-        self.SHOULD_EXIT = False
+        self.should_exit = False
+
+    def enter_next_level(self):
+        pyautogui.press('g')
+
+    def character_move_to(self, pos):
+        random_action = randint(0, 5)
+        pyautogui.moveTo(pos[0], pos[1], random.uniform(0.1, 0.3))
+        if random_action == 0:
+            pyautogui.press('space')
+        else:
+            pyautogui.mouseDown(button='right')
+            self.waitRandomizedTime(0.5, 1.5)
+            pyautogui.mouseUp(button='right')
 
     def exit_chaos(self):
         self.moveToClick(self.exit_range())
         self.waitRandomizedTime(0.5, 0.8)
         self.check_ok()
         self.IN_CITY = True
-        self.waitRandomizedTime(10, 12)
+
+    def print_state(self, remaining_time):
+        print("============================================================================================================")
+        print("Remaining seconds: ", remaining_time)
+        print("IN_CITY\tMOVE\texit\tlevel2\tcast\tdirec")
+        print(self.IN_CITY, self.MOVE, self.should_exit, self.is_level_2, self.disable_cast, self.target_direction, sep='\t')
+        print("============================================================================================================")
 
     ## do chaos dungeon after finding one
     def do_chaos(self):
         start_time = time.time()
+        force_cast_count = 0
+        state_count = 10
         while not self.IN_CITY and time.time() - start_time < self.TIMEOUT:
+            if state_count >= 10:
+                self.print_state(self.TIMEOUT - (time.time() - start_time))
+                state_count = 0
+            else:
+                state_count += 1
             if not self.MOVE:
                 self.MOVE = True
-                start_scan = Thread(target = self.scan_for_portal)
-                start_scan.start()
+                Thread(target = self.scan_for_portal).start()
+                Thread(target = self.scan_minimap).start()
             if self.is_in_game():
-                self.check_resurection()
-                self.cast_spell(self.all_spells)
+                if self.should_exit:
+                    if not self.is_level_2:
+                        print("Entering next level.")
+                        self.enter_next_level()
+                    else:
+                        print("Exiting dungeon.")
+                        self.exit_chaos()
+                    self.should_exit = False
+                    continue
+                if self.target_direction:
+                    print("Heading to ", self.target_direction)
+                    self.character_move_to(self.target_direction)
+                if not self.disable_cast or force_cast_count >= 10:
+                    force_cast_count = 0
+                    self.cast_spell(self.all_spells)
+                else:
+                    force_cast_count += 1
+                    print("cast disabled...", force_cast_count)
             else:
                 self.not_in_game_count += 1
                 print("Not in game! Perhaps loading..." + str(self.not_in_game_count))
-            if self.SHOULD_EXIT:
-                self.exit_chaos()
+        if not self.IN_CITY:
+            print("Exiting dungeon due to timeout.")
+            self.exit_chaos()
             
 
 def capture_screen():
@@ -257,6 +378,8 @@ def kill_bot():
     Thread(target = capture_screen, args = ()).start()
     while True:
         if keyboard.is_pressed("del"):
+            print("RUN_COUNT:", Lost_bot.RUN_COUNT)
+            print("RESURRECT_COUNT:", Lost_bot.RESURRECT_COUNT)
             os._exit(1)
         time.sleep(1)
 
@@ -268,68 +391,27 @@ def run_bot():
     total_time = int(input("How long do you want to run? (minutes)"))
     start_time = time.time()
     Esc = True
-    count = 10
+    repair_count = 10  # Start with repairing.
     while True and (time.time() - start_time) / 60 < total_time:
         bot = Lost_bot()
         if Esc:
             Thread(target = kill_bot, args = ()).start()
             Esc = False
-        if count >= 10:
-            count = 0
+        if repair_count >= 10:
+            repair_count = 0
+            print("Reparing...")
             bot.repair_items()
         bot.waitRandomizedTime(1, 2)
+        print("Entering chaos...")
         bot.alt_q_chaos()
         bot.waitRandomizedTime(5, 8)
         bot.do_chaos()
-        count += 1
-
-def draw_rectangle(loc, color):
-    print(loc)
-    start_point = (loc[0] - 10, loc[1] - 10)
-    end_point = (loc[0] + 10, loc[1] + 10)
-    thickness = 2
-    cv2.rectangle(screenshot, start_point, end_point, color, thickness)
-
-def draw_line(start_loc, end_loc):
-    cv2.line(screenshot, start_loc, end_loc, (255, 0, 0), 2)
-
-def run_unit_test():
-    bot = Lost_bot()
-    global screenshot
-    screenshot = cv2.imread("images\\screenshot_test_2.jpg", cv2.IMREAD_UNCHANGED)
-    # find_on_minimap_test(bot, Lost_bot.minimap_boss_path)
-    # find_on_minimap_test(bot, Lost_bot.minimap_elite_path)
-    # find_on_minimap_test(bot, Lost_bot.minimap_self_east_path)
-
-    # my_loc = bot.find_on_minimap(Lost_bot.minimap_self_north_path, 0.92, cv2.TM_CCORR_NORMED)
-    my_loc = bot.get_minimap_center()
-    boss_loc = bot.find_on_minimap(Lost_bot.minimap_boss_path, 0.95, cv2.TM_CCORR_NORMED)
-    elite_loc = bot.find_on_minimap(Lost_bot.minimap_elite_path, 0.95, cv2.TM_CCORR_NORMED)
-
-    if my_loc:
-        print("self found.")
-        draw_rectangle(my_loc, (255, 0, 0))
-    if boss_loc:
-        print("boss found.")
-        draw_rectangle(boss_loc, (0, 255, 0))
-    if elite_loc:
-        print("elite found.")
-        draw_rectangle(elite_loc, (0, 0, 255))
-
-    print(my_loc, boss_loc, elite_loc)
-
-    my_real_loc = bot.get_center_loc()
-    if my_loc and boss_loc:
-        boss_real_dir = bot.get_heading_direction(my_loc, boss_loc)
-        draw_line(my_real_loc, boss_real_dir)
-    if my_loc and elite_loc:
-        elite_real_dir = bot.get_heading_direction(my_loc, elite_loc)
-        draw_line(my_real_loc, elite_real_dir)
-
-    cv2.imshow("Heading!", screenshot)
-    cv2.waitKey(0)
+        repair_count += 1
+        Lost_bot.RUN_COUNT += 1
+        bot.waitRandomizedTime(15, 20)
+    print("Whole run complete.")
+    os._exit(1)
 
         
 if __name__ == '__main__':
-    # run_bot()
-    run_unit_test()
+    run_bot()
---
+++ test.py
@@ -0,0 +1,55 @@
+import pyautogui, time, random, cv2, math, keyboard, os, numpy as np
+import time
+from random import randint
+from threading import Thread
+from window_capture import WindowCapture
+pyautogui.FAILSAFE = False
+from config import configure_config
+
+from main import Lost_bot
+
+def run_unit_test():
+    global screenshot
+    screenshot = cv2.imread("images\\screenshot_test_7.jpg", cv2.IMREAD_UNCHANGED)
+    bot = Lost_bot(screenshot)
+
+    my_loc = bot.get_minimap_center()
+    boss_loc = bot.find_on_minimap(Lost_bot.minimap_boss_path, 0.65, cv2.TM_CCOEFF_NORMED)
+    elite_loc = bot.find_on_minimap(Lost_bot.minimap_elite_path, 0.65, cv2.TM_CCOEFF_NORMED)
+
+    if my_loc:
+        print("self found.")
+        draw_rectangle(my_loc, (255, 0, 0))
+    if boss_loc:
+        print("boss found. Distance:", bot.calc_distance(my_loc, boss_loc))
+        draw_rectangle(boss_loc, (0, 255, 0))
+    if elite_loc:
+        print("elite found. Distance:", bot.calc_distance(my_loc, elite_loc))
+        draw_rectangle(elite_loc, (0, 0, 255))
+
+    print(my_loc, boss_loc, elite_loc)
+
+    my_real_loc = bot.get_center_loc()
+    if my_loc and boss_loc:
+        boss_real_dir = bot.get_heading_direction(my_loc, boss_loc)
+        draw_line(my_real_loc, boss_real_dir)
+    if my_loc and elite_loc:
+        elite_real_dir = bot.get_heading_direction(my_loc, elite_loc)
+        draw_line(my_real_loc, elite_real_dir)
+
+    cv2.imshow("Heading!", screenshot)
+    cv2.waitKey(0)
+
+def draw_rectangle(loc, color):
+    global screenshot
+    print(loc)
+    start_point = (loc[0] - 10, loc[1] - 10)
+    end_point = (loc[0] + 10, loc[1] + 10)
+    thickness = 2
+    cv2.rectangle(screenshot, start_point, end_point, color, thickness)
+
+def draw_line(start_loc, end_loc):
+    cv2.arrowedLine(screenshot, start_loc, end_loc, (255, 0, 0), 2)
+
+if __name__ == '__main__':
+    run_unit_test()
